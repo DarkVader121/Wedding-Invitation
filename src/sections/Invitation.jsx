e0fb6e7af258dfa75963eab0464043f3e1653c7c
Erron John <erronjohn@gmail.com>
2026-08-28 23:35:34 +0800

    tailwind set up fix and font

--- src/sections/Invitation.jsx
+++ src/sections/Invitation.jsx
@@ -4,12 +4,15 @@ const Invitation = () => {
         <>
             <section className="hero">
                 <div className="container">
-                    <div className="flex f-column justify-content-center align-items-center">
-                        <p>You are cordially invited to the <br /> wedding of</p>
+                    <div className="flex flex-col justify-center items-center">
+                        <p className="text-primary text-center">You are cordially invited to the <br /> wedding of</p>
+                        <h1 className="mt-5 text-7xl text-primary">Erron</h1> 
+                        <p className="text-3xl text-primary">and</p> 
+                        <h2 className="text-7xl text-primary">Deakin</h2>
                     </div>
                 </div>
             </section>
-      </>
+        </>
     );
 }
 
