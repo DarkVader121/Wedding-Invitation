262aa5383a98a4e4e13c0e5634e1dce6c5c6a3c1
Erron John <erronjohn@gmail.com>
2026-08-29 00:29:52 +0800

    invitation section

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './variables.css'
+import './assets/css/custom.css'
+import './assets/css/overides.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
--- src/sections/Invitation.jsx
+++ src/sections/Invitation.jsx
@@ -9,7 +9,31 @@ const Invitation = () => {
                         <h1 className="mt-5 text-7xl text-primary">Erron</h1> 
                         <p className="text-3xl text-primary">and</p> 
                         <h2 className="text-7xl text-primary">Deakin</h2>
+                    
+                    <div className="mt-10 grid grid-cols-3 justify-center items-center">
+                        <div className="flex flex-col justify-center items-center">
+                            <hr className="w-3/4 text-primary" />
+                            <p className="text-center text-lg">Wednesday</p>
+                            <hr className="w-3/4 text-primary" />
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <p className="lead">JANUARY</p>
+                            <p className="text-5xl text-primary">27</p>
+                            <p className="lead">2026</p>
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <hr className="w-3/4 text-primary"/>
+                            <p className="text-center text-lg">9:30 AM</p>
+                            <hr className="w-3/4 text-primary"/>
+                        </div>
                     </div>
+
+                    <div className="mt-10 wi-title-container">
+                        <h3>The</h3>
+                        <p>COUNTDOWN</p>
+                    </div>
+                    </div>
+               
                 </div>
             </section>
         </>
