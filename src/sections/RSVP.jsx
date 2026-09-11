2a74b47dd2fda1bf3a2a47f85543427ff30dd832
Erron John <erronjohn@gmail.com>
2026-09-11 20:49:47 +0800

    rsvp

--- src/sections/RSVP.jsx
+++ src/sections/RSVP.jsx
@@ -6,7 +6,7 @@ const RSVP = () => {
     return (
         <>
             <section>
-                <div className="container">
+                <div>
                     <div className="flex flex-col justify-center items-center">
                         {/* <div className="wi-title-container mt-1">
                             <h3>Heartfelt </h3>
@@ -18,7 +18,7 @@ const RSVP = () => {
                         </p> */}
 
                         <img src={cutoffs}  alt="" className="w-[50%] object-cover h-[35px] mb-5" />
-                        <iframe className='h-[500px] w-[100%]' src="https://docs.google.com/forms/d/e/1FAIpQLSfzOlHthcf19gSM0YMuDVqAzuNlpleQxBOnfczvgdM8zmtsvQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+                        <iframe className='h-[600px] w-[100%]' src="https://docs.google.com/forms/d/e/1FAIpQLSfzOlHthcf19gSM0YMuDVqAzuNlpleQxBOnfczvgdM8zmtsvQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                     </div>
                 </div>
             </section>
