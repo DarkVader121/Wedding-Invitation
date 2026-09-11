4b89c27fbed161b3166e540a3c15d32b928ec714
Erron John <erronjohn@gmail.com>
2026-09-11 20:47:26 +0800

    RSVP

--- src/sections/RSVP.jsx
+++ src/sections/RSVP.jsx
@@ -8,26 +8,17 @@ const RSVP = () => {
             <section>
                 <div className="container">
                     <div className="flex flex-col justify-center items-center">
-                        <div className="wi-title-container mt-1">
+                        {/* <div className="wi-title-container mt-1">
                             <h3>Heartfelt </h3>
                             <p>RSVP</p>
                         </div>
 
                         <p className="mt-7 mb-3 text-center">
                             We appreciate your pressence in our wedding day, kindly confirm your RSVP by reaching out to Erron or Deakin directly. 
-                        </p>
+                        </p> */}
 
-                        <img src={cutoffs}  alt="" className="w-[50%] object-cover h-[35px]" />
-                        <p className="mt-3 text-center">
-                            Call or Message 
-                        </p>
-                        <a href="tel:09061164928" className="text-center flex items-center gap-1 !text-secondary">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                            </svg>
-
-                            0906 116 4928
-                        </a>
+                        <img src={cutoffs}  alt="" className="w-[50%] object-cover h-[35px] mb-5" />
+                        <iframe className='h-[500px] w-[100%]' src="https://docs.google.com/forms/d/e/1FAIpQLSfzOlHthcf19gSM0YMuDVqAzuNlpleQxBOnfczvgdM8zmtsvQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
                     </div>
                 </div>
             </section>
