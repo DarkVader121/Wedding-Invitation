580e9b1e210eece72b176d93bd5954636cd4a877
Erron John <erronjohn@gmail.com>
2026-09-25 18:46:11 +0800

    feedback

--- src/sections/Details.jsx
+++ src/sections/Details.jsx
@@ -9,14 +9,13 @@ const Details = () => {
                         <h3>The</h3>
                         <p>Details</p>
                     </div>
-                    <h3 className="mt-7 text-2xl !mb-0">Ceremony</h3>
+                    <p className="mt-7 text-2xl !mb-0">Ceremony</p>
                     <p className="text-center text-lg">Assumption of Our Lady <br /> Shrine Parish, Dauis.</p>
 
-                     <h3 className="mt-7 text-2xl !mb-0">Reception</h3>
+                     <p className="mt-7 text-2xl !mb-0">Reception</p>
                     <p className="text-center text-lg">The Bellevue Resort <br /> Panglao.</p>
 
                 </div>
-              
             </div>
         </section>
       </>
